b8ab4a0ecf468e6d3fef77d4577de2f082817d02
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2019-12-05 23:10:18 +0700

    chore(cli): main

---
+++ cli.go
@@ -0,0 +1,24 @@
+package xgoinstall
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+)
+
+const cmdName = "x-go-install"
+
+// Run command
+func Run(argv []string, outStream, errStream io.Writer) error {
+	log.SetOutput(errStream)
+	log.SetPrefix(fmt.Sprintf("[%s] ", cmdName))
+	nameAndVer := fmt.Sprintf("%s (v%s rev:%s)", cmdName, version, revision)
+	fs := flag.NewFlagSet(nameAndVer, flag.ContinueOnError)
+	fs.SetOutput(errStream)
+	fs.Usage = func() {
+		fmt.Fprintf(fs.Output(), "Usage of %s:\n", nameAndVer)
+		fs.PrintDefaults()
+	}
+	return nil
+}
