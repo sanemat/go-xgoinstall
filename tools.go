60eec25fb44464f1c34aa565e4da48dd4d538713
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2019-12-09 21:03:16 +0700

    chore(tools): use x-go-install and import-list

--- tools.go
+++ tools.go
@@ -7,6 +7,8 @@ import (
 	_ "github.com/Songmu/goxz/cmd/goxz"
 	_ "github.com/git-chglog/git-chglog/cmd/git-chglog"
 	_ "github.com/motemen/gobump/cmd/gobump"
+	_ "github.com/sanemat/go-importlist/cmd/import-list"
+	_ "github.com/sanemat/go-xgoinstall/cmd/x-go-install"
 	_ "github.com/tcnksm/ghr"
 	_ "golang.org/x/lint/golint"
 	_ "golang.org/x/tools/cmd/goimports"
